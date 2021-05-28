14c3ff60277b8ec9d8e06b5e810760099ca75cd6
GuruLatukar <gururaj.l@shirishproductions.com>
2021-05-28 15:38:13 +0530

    Add client app files

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios"
+
+function App(props) {
+    return (
+      <div className="container mt-5 mb-3">
+        <h3>Spofiy Task </h3>
+      </div>
+    );
+}
+
+export default App;
